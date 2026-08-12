2ca175281b1430df49ef3e28a72f03b723791351
Aakriti43 <akriteessiilwal@gmail.com>
2026-08-12 13:25:55 +0545

    new code added

--- core/statements/urls.py
+++ core/statements/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('login/', views.user_login, name='user_login'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/', TemplateView.as_view(template_name='dashboard.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path("download/", views.download_statement, name="download_statement"),
     path('logout/', views.user_logout, name='logout'),
 ]
