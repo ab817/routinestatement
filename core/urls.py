a669abef1b5e3fb65c8b2dd5121dc3ab4cb8f593
Ankit Bhattarai <ablive817@gmail.com>
2026-08-25 13:13:14 +0545

    runable app - postgres

--- core/urls.py
+++ core/urls.py
@@ -16,16 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('statements.urls')),
-    path("api/", include("statements_api.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # OAuth2 Provider
+    path(
+        'oauth/',
+        include('oauth2_provider.urls', namespace='oauth2_provider')
+    ),
+    path("api/v1/", include("statements_api.urls")),
 ]
 
